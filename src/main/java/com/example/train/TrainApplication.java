2f22976982bf1df01324e3e037ef754293b33bb2
fierro1994 <fierrobai94@gmail.com>
2024-06-26 08:53:08 +0300

    docker-compose.yaml fix

--- src/main/java/com/example/train/TrainApplication.java
+++ src/main/java/com/example/train/TrainApplication.java
@@ -2,12 +2,19 @@ package com.example.train;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrainApplication {
+public class TrainApplication  extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TrainApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainApplication.class, args);
 	}
 
 }
+

8f4bdf91a18eb89c9ef43c9dee88d611af3cc3ad
fierro1994 <fierrobai94@gmail.com>
2024-06-27 22:34:10 +0300

    add count for one test

--- src/main/java/com/example/train/service/DataInitializer.java
+++ src/main/java/com/example/train/service/DataInitializer.java
@@ -4,6 +4,7 @@ import com.example.train.entity.RoleName;
 import com.example.train.entity.User;
 import com.example.train.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,8 @@ public class DataInitializer {
 
     @Autowired
     private AuthService authService;
-
+    @Value("${app.adminPassword}")
+    private String adminPassword;
     @Bean
     public CommandLineRunner initData(UserRepository userRepository) {
         return args -> {
@@ -21,7 +23,7 @@ public class DataInitializer {
                 User admin = new User();
                 admin.setUsername("fierro1994");
                 admin.setEmail("26roma261994@mail.ru");
-                admin.setPassword("DD52joexcmk!");
+                admin.setPassword(adminPassword);
                 authService.register(admin, RoleName.ROLE_ADMIN);
                 System.out.println("Админ создан");
             }
